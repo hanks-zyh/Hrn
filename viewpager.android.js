ac3d6677cf4cd839c77d2ffc07bb9f08ea361232
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-25 17:43:50 +0800

    add viewpager.android.js

--- viewpager.android.js
+++ viewpager.android.js
@@ -13,10 +13,29 @@ var {
   ViewPagerAndroid,
 } = React;
 
+BackAndroid.addEventListener('hardwareBackPress', function() {
+  if(_navigator == null){
+    return false;
+  }
+  if(_navigator.getCurrentRoutes().length === 1){
+    return false;
+  }
+  _navigator.pop();
+  return true;
+});
+
+var _navigator ;
+
 var ViewPager  = React.createClass({
-    render: function(){
-        return <Text>ddd</Text>
-    }
+  getInitialState: function(){
+    _navigator = this.props.navigator;
+    return {};
+  },
+  render: function(){
+      return (
+        <Text>ddd</Text>
+      );
+  }
 });
 
 var styles = StyleSheet.create({
